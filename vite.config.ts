37b8b31b4159e3d8d70456c369d316db24b65460
re047988 <re047988@qintess.com>
2025-06-04 20:51:27 -0300

    Ajustes de bugs /2

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,21 @@
-// vite.config.js
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['@chakra-ui/system']
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@chakra-ui/system': path.resolve(__dirname, './node_modules/@chakra-ui/react')
     }
+  },
+  optimizeDeps: {
+    include: [
+      '@chakra-ui/react',
+      '@emotion/react',
+      '@emotion/styled',
+      'framer-motion'
+    ]
   }
-});
+})
